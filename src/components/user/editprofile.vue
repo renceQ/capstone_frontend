6367352911a02495b8e8f9717a0bf6a62b401d31
RenceQ <130763065+renceQ@users.noreply.github.com>
2024-01-12 14:16:56 +0800

    update profile done

--- src/components/user/editprofile.vue
+++ src/components/user/editprofile.vue
@@ -40,6 +40,8 @@
 
 
                 <!--products container-->
+             
+              
                 <div>
                   <div class="container" style="margin-top: 20px;">
                     <nav class="neumorphic-navbars" style="width: 945px; margin-left: 200px; z-index: 10;">
@@ -49,110 +51,75 @@
                           <div>
                             
                           </div>
-            
                           <div class="card mb-4 mb-xl-0" style="margin-top:8px; width: 880px;">
                             <div class="card-header">Profile Picture</div>
-                <div class="card-body text-center">
-                    <!-- Profile picture image-->
+                              <div class="card-body text-center">
                    
-                    <img class="img-account-profile rounded-circle mb-2" v-if="selectedImage" :src="selectedImage" alt="" style="box-shadow: 10px 10px 30px #eeecec, -5px -5px 20px #ffffff;">
-<img class="img-account-profile rounded-circle mb-2" v-else-if="info.length > 0" :src="info[0].profile_picture" alt="" style="box-shadow: 10px 10px 30px #eeecec, -5px -5px 20px #ffffff;">
-<img class="img-account-profile rounded-circle mb-2" v-else :src="'path_to_default_image.jpg'" alt="" style="box-shadow: 10px 10px 30px #eeecec, -5px -5px 20px #ffffff;">
+                                  <img class="img-account-profile rounded-circle mb-2" v-if="selectedImage" :src="selectedImage" alt="" style="box-shadow: 10px 10px 30px #eeecec, -5px -5px 20px #ffffff;">
+                                  <img class="img-account-profile rounded-circle mb-2" v-else-if="info.length > 0" :src="info[0].profile_picture" alt="" style="box-shadow: 10px 10px 30px #eeecec, -5px -5px 20px #ffffff;">
+                                  <img class="img-account-profile rounded-circle mb-2" v-else :src="'path_to_default_image.jpg'" alt="" style="box-shadow: 10px 10px 30px #eeecec, -5px -5px 20px #ffffff;">
 
-<!-- Profile picture help block -->
-<div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
+                          <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
+                        <input type="file" style="border: 0; width:29%; border-radius:5px; background-color:rgb(220, 220, 223)" class="neumorphic-button" accept="image/*" @change="updateEditImage" required>
+                       </div>
+                    </div>
 
-<!-- Profile picture upload button -->
-<input type="file" style="border: 0; width:29%; border-radius:5px; background-color:rgb(220, 220, 223)" class="neumorphic-button" accept="image/*" @change="updateEditImage" required>
-               </div>
-               </div>
-
-              
-            <!-- Account details card-->
+                    <form @submit.prevent="updateProfile">
             <div class="card mb-4" style="height:700px;">
                 <div class="card-header">Account Details</div>
                 <div class="card-body">
-               
-                        <!-- Form Group (username)-->
-                        <div style="margin-left: 10px;">
-                        <div class="mb-3" >
-                            <label class="small mb-1" for="inputUsername">Username (how your name will appear to other users on the site)</label>
-                            <input v-if="info.length > 0" class="form-control" id="inputUsername" type="text" placeholder="Enter your username" v-model="info[0].showed_username">
+                    <div style="margin-left: 10px;">
+                      <div class="mb-3">
+                        <label class="small mb-1" for="inputUsername">Username</label>
+                        <input v-if="info.length > 0" class="form-control" id="inputUsername" type="text" placeholder="Enter your username" v-model="info[0].showed_username">
+                      </div>
+                
+                      <div class="row gx-3 mb-3">
+                        <div class="col-md-6">
+                          <label class="small mb-1" for="inputFirstName">Contact</label>
+                          <input v-if="info.length > 0" class="form-control" id="inputFirstName" type="text" placeholder="Enter your contact" v-model="info[0].contact">
                         </div>
-                        <!-- Form Row-->
-                        <div class="row gx-3 mb-3">
-                            <!-- Form Group (first name)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputFirstName">Contact</label>
-                                <input v-if="info.length > 0" class="form-control" id="inputFirstName" type="text" placeholder="Enter your contact" v-model="info[0].contact">
-                            </div>
-                            <!-- Form Group (last name)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputLastName">Address</label>
-                                <input v-if="info.length > 0" class="form-control" id="inputLastName" type="text" placeholder="Enter your Address" v-model="info[0].address">
-                            </div>
+                        <div class="col-md-6">
+                          <label class="small mb-1" for="inputLastName">Address</label>
+                          <input v-if="info.length > 0" class="form-control" id="inputLastName" type="text" placeholder="Enter your Address" v-model="info[0].address">
                         </div>
-                        <!-- Form Group (email address)-->
-                        <div class="mb-3">
-                            <label class="small mb-1" for="inputEmailAddress">Other Information (Delivery location or Customer information):</label>
-                            <input class="form-control" id="inputEmailAddress" type="text" placeholder="Enter your other information" v-model="info[0].other_info">
+                      </div>
+                
+                      <div class="mb-3">
+                        <label class="small mb-1" for="inputEmailAddress">Other Information</label>
+                        <input class="form-control" id="inputEmailAddress" type="text" placeholder="Enter your other information" v-model="info[0].other_info">
+                      </div>
+                
+                      <div class="row gx-3 mb-3">
+                        <div class="col-md-6">
+                          <label class="small mb-1" for="input_name">Name</label>
+                          <input class="form-control" id="input_name" type="text" placeholder="Enter your name" v-model="info[0].legit_name">
                         </div>
-                        <!-- Form Row-->
-                        <div class="row gx-3 mb-3">
-                            <!-- Form Group (phone number)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="input_name">Name</label>
-                                <input class="form-control" id="input_name" type="tel" placeholder="Enter your name" v-model="info[0].legit_name">
-                            </div>
-                            <!-- Form Group (birthday)-->
-                            <div class="col-md-6">
-                              <label>
-                                <a4 style="font-size: 14px;">&nbsp;&nbsp; Gender:</a4>
-                              </label>
-                              <br>
-                              <label style="margin-top: 8px; margin-left:10px;">
-                                <input type="radio" v-model="info[0].gender" value="male"> Male
-                              </label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                              
-                              <label>
-                                <input type="radio" v-model="info[0].gender" value="female"> Female
-                              </label>    
-                              
-                              <div>
-                                <label for="province">Province:</label>
-                                <select id="province" v-model="selectedProvince" @change="onProvinceChange">
-                                  <option v-for="province in provinces" :key="province.id" :value="province.id">{{ province.name }}</option>
-                                </select>
-                            
-                                <label for="city" v-if="selectedProvince">City:</label>
-                                <select id="city" v-model="selectedCity" v-if="selectedProvince" @change="onCityChange">
-                                  <option v-for="city in cities" :key="city.id" :value="city.id">{{ city.name }}</option>
-                                </select>
-                            
-                                <label for="barangay" v-if="selectedCity">Barangay:</label>
-                                <select id="barangay" v-model="selectedBarangay" v-if="selectedCity">
-                                  <option v-for="barangay in barangays" :key="barangay.id" :value="barangay.id">{{ barangay.name }}</option>
-                                </select>
-                              </div>
-                            
-
-                            </div>
-
-                          </div>
+                        <div class="col-md-6">
+                          <label>Gender:</label>
+                          <br>
+                          <label style="margin-top: 8px; margin-left:10px;">
+                            <input type="radio" v-model="info[0].gender" value="male"> Male
+                          </label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                          <label>
+                            <input type="radio" v-model="info[0].gender" value="female"> Female
+                          </label>
                         </div>
-                        <!-- Save changes button-->
-                        <button class="btn btn-primary" style="margin-left: 350px; margin-top: 30px; width: 150px;" type="submit">Save changes</button>
-              
-                            </div>
-
-                          </div>
-                        </li>
-                      </ul>
-                      
-                    </nav>
+                      </div>
+                      <button class="btn btn-primary" style="margin-left: 350px; margin-top: 30px; width: 150px;">Update Profile</button>
+                    </div>
                   </div>
                 </div>
-            
+              </form>
+                       
+
+                        </li>
+                      </ul>  
+                    </nav>
+                    
+                  </div>
+                </div>
+           
 
 
                 <!--cancel modal-->
@@ -178,102 +145,6 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      selectedProvince: null,
-      selectedCity: null,
-      selectedBarangay: null,
-      provinces: [
-        { id: 1, name: 'Abra' },
-  { id: 2, name: 'Agusan del Norte' },
-  { id: 3, name: 'Agusan del Sur' },
-  { id: 4, name: 'Aklan' },
-  { id: 5, name: 'Albay' },
-  { id: 6, name: 'Antique' },
-  { id: 7, name: 'Apayao' },
-  { id: 8, name: 'Aurora' },
-  { id: 9, name: 'Basilan' },
-  { id: 10, name: 'Bataan' },
-  { id: 11, name: 'Batanes' },
-  { id: 12, name: 'Batangas' },
-  { id: 13, name: 'Benguet' },
-  { id: 14, name: 'Biliran' },
-  { id: 15, name: 'Bohol' },
-  { id: 16, name: 'Bukidnon' },
-  { id: 17, name: 'Bulacan' },
-  { id: 18, name: 'Cagayan' },
-  { id: 19, name: 'Camarines Norte' },
-  { id: 20, name: 'Camarines Sur' },
-  { id: 21, name: 'Camiguin' },
-  { id: 22, name: 'Capiz' },
-  { id: 23, name: 'Catanduanes' },
-  { id: 24, name: 'Cavite' },
-  { id: 25, name: 'Cebu' },
-  { id: 26, name: 'Compostela Valley' },
-  { id: 27, name: 'Cotabato' },
-  { id: 28, name: 'Davao de Oro' },
-  { id: 29, name: 'Davao del Norte' },
-  { id: 30, name: 'Davao del Sur' },
-  { id: 31, name: 'Davao Occidental' },
-  { id: 32, name: 'Davao Oriental' },
-  { id: 33, name: 'Dinagat Islands' },
-  { id: 34, name: 'Eastern Samar' },
-  { id: 35, name: 'Guimaras' },
-  { id: 36, name: 'Ifugao' },
-  { id: 37, name: 'Ilocos Norte' },
-  { id: 38, name: 'Ilocos Sur' },
-  { id: 39, name: 'Iloilo' },
-  { id: 40, name: 'Isabela' },
-  { id: 41, name: 'Kalinga' },
-  { id: 42, name: 'La Union' },
-  { id: 43, name: 'Laguna' },
-  { id: 44, name: 'Lanao del Norte' },
-  { id: 45, name: 'Lanao del Sur' },
-  { id: 46, name: 'Leyte' },
-  { id: 47, name: 'Maguindanao' },
-  { id: 48, name: 'Marinduque' },
-  { id: 49, name: 'Masbate' },
-  { id: 50, name: 'Misamis Occidental' },
-  { id: 51, name: 'Misamis Oriental' },
-  { id: 52, name: 'Mountain Province' },
-  { id: 53, name: 'Negros Occidental' },
-  { id: 54, name: 'Negros Oriental' },
-  { id: 55, name: 'Northern Samar' },
-  { id: 56, name: 'Nueva Ecija' },
-  { id: 57, name: 'Nueva Vizcaya' },
-  { id: 58, name: 'Occidental Mindoro' },
-  { id: 59, name: 'Oriental Mindoro' },
-  { id: 60, name: 'Palawan' },
-  { id: 61, name: 'Pampanga' },
-  { id: 62, name: 'Pangasinan' },
-  { id: 63, name: 'Quezon' },
-  { id: 64, name: 'Quirino' },
-  { id: 65, name: 'Rizal' },
-  { id: 66, name: 'Romblon' },
-  { id: 67, name: 'Samar' },
-  { id: 68, name: 'Sarangani' },
-  { id: 69, name: 'Siquijor' },
-  { id: 70, name: 'Sorsogon' },
-  { id: 71, name: 'South Cotabato' },
-  { id: 72, name: 'Southern Leyte' },
-  { id: 73, name: 'Sultan Kudarat' },
-  { id: 74, name: 'Sulu' },
-  { id: 75, name: 'Surigao del Norte' },
-  { id: 76, name: 'Surigao del Sur' },
-  { id: 77, name: 'Tarlac' },
-  { id: 78, name: 'Tawi-Tawi' },
-  { id: 79, name: 'Zambales' },
-  { id: 80, name: 'Zamboanga del Norte' },
-  { id: 81, name: 'Zamboanga del Sur' },
-  { id: 82, name: 'Zamboanga Sibugay' }
-      ],
-      cities: [
-        { id: 1, name: 'Quezon City', provinceId: 1 },
-        // Add other cities
-      ],
-      barangays: [
-        { id: 1, name: 'Barangay 1', cityId: 1 },
-        // Add other barangays
-      ],
-
       selectedGender: '',
       selectedImage: null,
         isNavbarHidden: false,
@@ -298,6 +169,7 @@ export default {
       ],
       hideStatus: false,
       selectedInfo: null,
+      image: null,
       
     };
   },
@@ -323,27 +195,33 @@ export default {
   },
 
   methods: {
+    async updateProfile() {
+    try {
+        const userId = this.info[0].id; // Assuming you have access to the user's ID
+        const response = await axios.post(`/updateProfile/${userId}`, {
+            showed_username: this.info[0].showed_username,
+            contact: this.info[0].contact,
+            address: this.info[0].address,
+            other_info: this.info[0].other_info,
+            legit_name: this.info[0].legit_name,
+            gender: this.info[0].gender,
+            // Add other properties as needed
+        });
 
-    onProvinceChange() {
-      this.selectedCity = null;
-      this.selectedBarangay = null;
-    },
-    onCityChange() {
-      this.selectedBarangay = null;
-    },
-    updateEditImage(event) {
-    // Get the selected file from the input
-    const file = event.target.files[0];
-
-    // Check if a file is selected
-    if (file) {
-      // Display the selected image
-      this.selectedImage = URL.createObjectURL(file);
-      
-      // You can also send the file to the server for processing if needed
-      // Example: this.uploadImageToServer(file);
+        if (response.status === 200) {
+            console.log('Profile updated successfully');
+        } else {
+            console.error('Error updating profile');
+        }
+    } catch (error) {
+        console.error('Error updating profile:', error);
     }
-  },
+},
+    updateEditImage(event) {
+      // Handle image selection
+      const file = event.target.files[0];
+      this.selectedImage = URL.createObjectURL(file);
+    },
    async deletehistory(id) {
   try {
     const confirmed = window.confirm('Are you sure you want to delete this record?');
