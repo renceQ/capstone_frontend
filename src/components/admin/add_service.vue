a0a54efe799cd37f7878874014d7d7f228ee5d0d
RenceQ <130763065+renceQ@users.noreply.github.com>
2024-02-12 02:24:16 +0800

    add service

--- src/components/admin/add_service.vue
+++ src/components/admin/add_service.vue
@@ -46,21 +46,21 @@
 
 
             <v-dialog v-model="dialogss" max-width="500px">
-                <form @submit.prevent="saveBooking" class="container">
+                <form @submit.prevent="saveService" class="container">
                   <v-card>
                     <br>
                     <v-card-title class="headline" style="margin-left: 155px; font-size: 14px; font-weight:900;  ">ADD<span style="font-size: 22px;  font-weight:100;">&nbsp;&nbsp;|&nbsp;</span><span style="font-weight:400; font-family: 'WindSong', cursive; font-size:33px;">Services</span></v-card-title>
                     <v-card-text>
               
                       <div class="form-group">
-                        <label for="service_title">Service Title:</label>
-                        <input type="text" class="search-input" style="width:100%; height:45px;" placeholder="Service Title" v-model="service" required>
+                        <label for="service">Service Title:</label>
+                        <input type="text" class="search-input" style="width:100%; height:45px; border:none;" placeholder="Service Title" v-model="service" required>
                       </div>
                       <br>
                       <div class="form-row">
                         <div class="form-group col-md-6">
-                          <label for="file_upload">Upload File:</label>
-                          <input type="file" id="file_upload" class="neumorphic-button" style="width: 110%; background-color: white; border-radius: 3px;" @change="handleFileChange" accept="image/*" required>
+                          <label for="image">Upload File:</label>
+                          <input type="file" id="image" class="neumorphic-button" style="width: 110%; background-color: white; border-radius: 3px;" @change="handleFileChange" accept="image/*" required>
                         </div>
                         <div>
                           <img v-if="imageUrl" :src="imageUrl" alt="Uploaded Image" style="position:absolute;width: 120px; height: 120px; right:11%;">
@@ -69,47 +69,120 @@
                       
                       
                       <br><br>
-                      <div class="form-row">
-                        <div class="form-group col-md-6">
-                          <label for="location">Location:</label>
-                          <input type="text" class="search-input" style="width:206%;height:60%" placeholder="Location" v-model="location" required>
+                      <div>
+                        <div class="form-row">
+                          <div class="form-group col-md-6">
+                            <label for="low_pricing">Lowest Price:</label>
+                            <input
+                              type="number"
+                              inputmode="numeric"
+                              class="search-input"
+                              style="width: 206%; height: 60%"
+                              placeholder="Lowest price"
+                              v-model="low_pricing"
+                              required
+                            />
+                          </div>
+                        </div>
+                    
+                        <div class="form-row">
+                          <div class="form-group col-md-6">
+                            <label for="high_pricing">Highest Price:</label>
+                            <input
+                              type="number"
+                              inputmode="numeric"
+                              class="search-input"
+                              style="width: 206%; height: 60%"
+                              placeholder="High price"
+                              v-model="high_pricing"
+                              required
+                            />
+                          </div>
                         </div>
                       </div>
               
                       <div class="form-row">
                         <div class="form-group col-md-6">
-                          <label for="event_description">Description:</label>
-                          <textarea class="search-input" v-model="event_description" style="width:206%; height:60%;" placeholder="Other Description" required></textarea>
+                          <label for="information">Service Information:</label>
+                          <textarea class="search-input" v-model="information" style="width:206%; height:100%;" placeholder="service offers" required></textarea>
                         </div>
                       </div>
               
                       <br><br>
-                      <h3 style="margin-left: 95px; font-size: 14px; font-weight:900;" >CONTACT<span style="font-size: 22px;  font-weight:100;">&nbsp;&nbsp;|&nbsp;</span><span style="font-weight:400; font-family: 'WindSong', cursive; font-size:33px;">Details</span></h3>
+                      <h3 style="margin-left: 95px; font-size: 14px; font-weight:900;" >REQUEST<span style="font-size: 22px;  font-weight:100;">&nbsp;&nbsp;|&nbsp;</span><span style="font-weight:400; font-family: 'WindSong', cursive; font-size:33px;">Options</span></h3>
                       <br>
+
                       <div class="form-row">
                         <div class="form-group col-md-6">
-                          <label for="name">Name:</label>
-                          <input type="text" class="search-input" style="width:206%;height:70%;" placeholder="Name" v-model="name" required>
+                          <label for="first_req">Request 1:</label>
+                          <input type="text" class="search-input" style="width:126%;height:70%; border:none;" placeholder="first request" v-model="first_req" >
+                        </div>
+                        <div class="form-group col-md-6" >
+                          <label for="first_price" style="margin-left:32%;">request price:</label>
+                          <input type="number" class="search-input"  inputmode="numeric" style="width:66%;height:70%; border:none;margin-left:32%;" placeholder="0000" v-model="first_price" >
                         </div>
                       </div>
               
                       <div class="form-row">
                         <div class="form-group col-md-6">
-                          <label for="email">Email:</label>
-                          <input type="email" class="search-input" style="width:206%;height:70%;" placeholder="Email Address" v-model="email" required>
+                          <label for="second_req">Request 2:</label>
+                          <input type="text" class="search-input" style="width:126%;height:70%;border:none;" placeholder="second request" v-model="second_req" >
+                        </div>
+                        <div class="form-group col-md-6" >
+                          <label for="second_price" style="margin-left:32%;">request price:</label>
+                          <input type="number" class="search-input"  inputmode="numeric" style="width:66%;height:70%; border:none;margin-left:32%;" placeholder="0000" v-model="second_price" >
                         </div>
                       </div>
-              
-                      <div class="form-group">
-                        <label for="phone">Phone:</label>
-                        <input type="tel" class="search-input" style="width:100%;height:50px;;" placeholder="09*********" v-model="phone" required>
+
+                      <div class="form-row">
+                        <div class="form-group col-md-6">
+                          <label for="third_req">Request 3:</label>
+                          <input type="text" class="search-input" style="width:126%;height:70%;border:none;" placeholder="third request" v-model="third_req" >
+                        </div>
+                        <div class="form-group col-md-6" >
+                          <label for="third_price" style="margin-left:32%;">request price:</label>
+                          <input type="number" class="search-input"  inputmode="numeric" style="width:66%;height:70%; border:none;margin-left:32%;" placeholder="0000" v-model="third_price" >
+                        </div>
                       </div>
-              
+
+                      <div class="form-row">
+                        <div class="form-group col-md-6">
+                          <label for="fourth_req">Request 4:</label>
+                          <input type="text" class="search-input" style="width:126%;height:70%;border:none;" placeholder="fourth request" v-model="fourth_req" >
+                        </div>
+                        <div class="form-group col-md-6" >
+                          <label for="fourth_price" style="margin-left:32%;">request price:</label>
+                          <input type="number" class="search-input"  inputmode="numeric" style="width:66%;height:70%; border:none;margin-left:32%;" placeholder="0000" v-model="fourth_price" >
+                        </div>
+                      </div>
+
+                      <div class="form-row">
+                        <div class="form-group col-md-6">
+                          <label for="fifth_req">Request 5:</label>
+                          <input type="text" class="search-input" style="width:126%;height:70%;border:none;" placeholder="fifth request" v-model="fifth_req" >
+                        </div>
+                        <div class="form-group col-md-6" >
+                          <label for="fifth_price" style="margin-left:32%;">request price:</label>
+                          <input type="number" class="search-input"  inputmode="numeric" style="width:66%;height:70%; border:none;margin-left:32%;" placeholder="0000" v-model="fifth_price" >
+                        </div>
+                      </div>
+
+                      <div class="form-row">
+                        <div class="form-group col-md-6">
+                          <label for="sixth_req">Request 6:</label>
+                          <input type="text" class="search-input" style="width:126%;height:70%;border:none;" placeholder="sixth request" v-model="sixth_req" >
+                        </div>
+                        <div class="form-group col-md-6" >
+                          <label for="sixth_price" style="margin-left:32%;">request price:</label>
+                          <input type="number" class="search-input"  inputmode="numeric" style="width:66%;height:70%; border:none;margin-left:32%;" placeholder="0000" v-model="sixth_price" >
+                        </div>
+                      </div>
+
                     </v-card-text>
                     <v-card-actions>
               
                       <div style="margin-bottom: 10px;">
-                        <button type="submit" class="neumorphic-button" style="margin-left: 10px; width: 320%; background-color: rgb(49, 48, 48); color: white;">Book Event</button>
+                        <button type="submit" class="neumorphic-button" style="margin-left: 15px; width: 275%; background-color: rgb(49, 48, 48); color: white;">Save Service</button>
                       </div>
               
                     </v-card-actions>
@@ -131,6 +204,24 @@
       info: [], 
       dialogss: false,
       imageUrl: null,
+      service:"", 
+      service:"", 
+      information:"", 
+      low_pricing:"", 
+      high_pricing:"", 
+      image:"", 
+      first_req:"", 
+      second_req:"", 
+      third_req:"", 
+      fourth_req:"", 
+      fifth_req:"", 
+      sixth_req:"", 
+      first_price:"", 
+      second_price:"", 
+      third_price:"", 
+      fourth_price:"", 
+      fifth_price:"", 
+      sixth_price:"", 
     }
   },
       components:{
@@ -143,6 +234,64 @@
           this.getInfo();
       },
       methods: {
+
+        async saveService() {
+  try {
+    // Display a confirmation dialog
+    const userConfirmed = window.confirm('Are you sure you want to save?');
+
+    // Check if the user clicked OK
+    if (userConfirmed) {
+      const ins = await axios.post("saveService", {
+        service: this.service,
+        information: this.information,
+        low_pricing: this.low_pricing,
+        high_pricing: this.high_pricing,
+        image: this.image,
+        first_req: this.first_req,
+        second_req: this.second_req,
+        third_req: this.third_req,
+        fourth_req: this.fourth_req,
+        fifth_req: this.fifth_req,
+        sixth_req: this.sixth_req,
+        first_price: this.first_price,
+        second_price: this.second_price,
+        third_price: this.third_price,
+        fourth_price: this.fourth_price,
+        fifth_price: this.fifth_price,
+        sixth_price: this.sixth_price,
+      });
+
+      // Clear form fields after successful save
+      this.event_title ="";
+      this.service ="";
+      this.information ="";
+      this.low_pricing ="";
+      this.high_pricing ="";
+      this.image ="";
+      this.first_req ="";
+      this.second_req ="";
+      this.third_req ="";
+      this.fourth_req ="";
+      this.fifth_req ="";
+      this.sixth_req ="";
+      this.first_price ="";
+      this.second_price ="";
+      this.third_price ="";
+      this.fourth_price ="";
+      this.fifth_price ="";
+      this.sixth_price ="";
+
+      // Emit event and perform other actions
+      this.$emit('data-saved');
+     
+      this.dialogss = false;
+    }
+    // If the user clicked Cancel, do nothing
+  } catch (error) {
+    console.error(error);
+  }
+},
         async getInfo() {
       try {
         const response = await axios.get('getservice');
